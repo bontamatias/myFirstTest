4236dc5e4bd512e35f4dba178781fa614f24e27b
Matias Bonta <mbonta@stu.naperville203.org>
2018-09-07 12:19:18 -0500

    Starting the Parking program

---
+++ Parking.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+public class Parking {
+
+	public static void main (String[] args) {
+	Stack<Integer> driveway = new Stack<>();
+	Stack<Integer> curbside = new Stack<>();
+
+
+
+
+	System.out.print("HELLO THERE");
+
+
+	}
+}
