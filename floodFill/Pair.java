a96982d69d7cdb14435d990209a3540ba24dfb3f
Matias Bonta <mbonta@stu.naperville203.or>
2018-09-13 17:23:25 +0000

    update

---
+++ floodFill/Pair.java
@@ -0,0 +1,17 @@
+
+public class Pair {
+	private int xCoord, yCoord;
+
+	public Pair (int xCoord, int yCoord) {
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+	}
+
+	public int getX () {
+		return xCoord;
+	}
+
+	public int getY () {
+		return yCoord;
+	}
+}
